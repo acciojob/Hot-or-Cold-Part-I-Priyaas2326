867073424b7af9828f766054d286c0d0ad0434ea
Priya Sharma <113659062+Priyaas2326@users.noreply.github.com>
2023-02-09 17:04:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //your code here
+const btn = document.getElementById("btn");
+const num = document.getElementById("num");
+
+btn.addEventListener("click", function() {
+  const randomNum = Math.floor(Math.random() * 41) - 20;
+  num.textContent = "Random Number: " + randomNum;
+});
+
 
