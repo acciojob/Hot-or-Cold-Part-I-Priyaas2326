61acf10643fc848d9c9df4dff5a15b507a725777
Priya Sharma <113659062+Priyaas2326@users.noreply.github.com>
2023-02-20 21:41:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 //your code here
-const btn = document.getElementById("btn");
-const num = document.getElementById("num");
+var btn = document.getElementById("btn");
+var numPtag = document.getElementById("num");
+var res = document.getElementById("respond");
+var inputValue = document.getElementById("guess").value;
 
-btn.addEventListener("click", function() {
-  const randomNum = Math.floor(Math.random() * 41) - 20;
-  num.textContent = "Random Number: " + randomNum;
-});
+function randomNumberGenerator() {
+	randomNum = Math.floor(Math.random() * 41) - 20;
+	document.getElementById("num").innerText = randomNum;
+	guessNum();
+	print();
+}
+
+function guessNum() {
+	input = Number(document.getElementById("guess").value);
+}
+
+function print() {
+	if(Math.abs(randomNum - input) <= 5){
+		document.getElementById("respond").innerText = "Hot";
+	}
+	else{
+		document.getElementById("respond").innerText = "Cold";
+	}
+}
+
+btn.addEventListener("click",randomNumberGenerator);
 
 
